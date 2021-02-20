96996e45dbee2ce95c290abac590fef77d494583
James Nguyen <minhkhoix01@gmail.com>
2021-02-20 15:59:54 -0800

    Accidentally deleted this file

---
+++ app/src/main/java/com/lazymail/comp2160project/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.comp_2160_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+    Button camera;
+    VideoView videoView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        openCamera();
+    }
+
+    // To access to camera
+    protected void openCamera(){
+        camera = (Button) findViewById(R.id.camera_btt);
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent();
+                    intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivity(intent);
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
