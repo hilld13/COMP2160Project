5ab7b2a68f23acb7142398104bb12c3aef209921
Donovan Hill <hilld13@mytru.ca>
2021-02-20 16:32:31 -0800

    Added planet activity

---
+++ app/src/main/java/com/lazymail/comp2160project/PlanetData.java
@@ -0,0 +1,14 @@
+package com.lazymail.comp2160project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlanetData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_planet_data);
+    }
+}
