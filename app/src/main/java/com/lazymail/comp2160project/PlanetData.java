07eebc5ca6d62a5c1da2708c6fd04da9311706f1
SpartanI117 <56614496+SpartanI117@users.noreply.github.com>
2021-02-21 16:23:50 -0800

    The title bar at the top of the activities now no longer show up.

--- app/src/main/java/com/lazymail/comp2160project/PlanetData.java
+++ app/src/main/java/com/lazymail/comp2160project/PlanetData.java
@@ -3,11 +3,14 @@ package com.lazymail.comp2160project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
 
 public class PlanetData extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_planet_data);
     }
