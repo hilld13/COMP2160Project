01864047f4de7370468f006260ec97c3e3f1c226
James Nguyen <minhkhoix01@gmail.com>
2021-03-08 16:30:37 -0800

    - Test displaying a simple arraylist to ListView with adapter

--- app/src/main/java/com/lazymail/comp2160project/PlanetData.java
+++ app/src/main/java/com/lazymail/comp2160project/PlanetData.java
@@ -1,51 +1,60 @@
 package com.lazymail.comp2160project;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.JsonReader;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import java.io.IOException;
-import java.nio.file.Files;
 
 import java.io.FileNotFoundException;
-
-import java.nio.file.Paths;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.List;
 
-import com.google.gson.*;
-
+import org.json.*;
+import org.json.simple.parser.JSONParser;
 
 public class PlanetData extends AppCompatActivity {
-    //Initializing variables
+    //Declaring variables.
+    JSONArray jsonArray = new JSONArray(); //contains data of planets.
+    ArrayList<String> arrayList = new ArrayList<String>();
     ListView listView;
-    ArrayList<String> planetList = new ArrayList<>();
-    String[] testList = {"1", "2", "3"};
-
+    ArrayAdapter<String> arrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // I can't make the list appear properly for some reason ! Need help !!
         setContentView(R.layout.activity_planet_data);
-        listView = findViewById(R.id.list_planet_view);
-
-        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, testList);
-        listView.setAdapter(itemsAdapter);
+        arrayList.add("1");
+        arrayList.add("2");
+        arrayList.add("3");
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,arrayList);
+        listView = (ListView) findViewById(R.id.listView_planets);
+        listView.setAdapter(arrayAdapter);
     }
 
 
-    //To add data from json file into array. (considering create a class as object to handle Planet data.)
-    protected void addData() throws IOException {
-        //Assign variables
-        Gson gson = new Gson();
-        String file = "src//main//assets//planetdata.json";
-        String json = "";
+    //READ JSON ARRAY
+    public void readJson(){
         try {
-            //read file
-            json =  new String(Files.readAllBytes(Paths.get(file)));
-            planetList = gson.fromJson(json, ArrayList.class);
+            FileReader reader = new FileReader("/src/main/assets/planetdata.json");
+            JSONParser parser = new JSONParser();
+            JSONArray jsonArray = (JSONArray) parser.parse(reader);
+
+            for (int i = 0; i < jsonArray.length(); i++){
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    arrayList.add(jsonObject.getString("Body"));
+            }
         } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }catch (IOException e) {
+            e.printStackTrace();
+        } catch (org.json.simple.parser.ParseException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
             e.printStackTrace();
         }
     }
